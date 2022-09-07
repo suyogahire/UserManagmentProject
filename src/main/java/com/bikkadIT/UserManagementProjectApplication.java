fc06852cdfe81f34be1046090353ccd283f06643
suyogahire <suyogahire54@.com>
2022-09-07 17:45:11 +0530

    UserManagmentApp

---
+++ src/main/java/com/bikkadIT/UserManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bikkadIT/UserManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikkadIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
